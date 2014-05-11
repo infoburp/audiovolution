9d986286b9a5fa7ac9e55e71a7ed62fc1ac0c6c6
Graeme Wolfendale <gwolfendale@gmail.com>
2014-05-11 01:37:35 +0100

    Create audiovolution.cpp

---
+++ audiovolution.cpp
@@ -0,0 +1,23 @@
+
+ //read input audio file
+ 
+ //create a random starting leader dna
+ 
+  sine, length, pitch, startpoint and volume.
+  
+ while (actualaccuracy<desiredaccuracy)
+ {
+
+ mutate dna
+ 
+ compare mutated dna audio to original audio
+ 
+ if it is more fit, overwrite the leader dna
+ 
+ }
+ 
+ //normalise
+ 
+ //output dna file and rendered audio
+ 
+ 
